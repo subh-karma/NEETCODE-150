f7c573317e16a58b4b071f7f4685ba96e01211ab
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2025-03-09 21:06:34 +0530

    Update Array Group Anagram.py

--- Array Group Anagram.py
+++ Array Group Anagram.py
@@ -1,7 +1,9 @@
-ana=defaultdict(list)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ana=defaultdict(list)
 
-        for i in strs:
-            sw=''.join(sorted(i))
+                for i in strs:
+                    sw=''.join(sorted(i))
             ana[sw].append(i)
 
         return list(ana.values())
